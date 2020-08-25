44b24ac0fbf954b9ae0e72faba726c535ed3f80e
derekyunwu <46233349+derekyunwu@users.noreply.github.com>
2020-08-24 19:01:12 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,16 @@ function showMoreSkills() {
     directionIcon.setAttribute("name", "chevron-up-outline");
   }
 }
+
+function badBackground(elem) {
+  var hobby = document.getElementById("hobby");
+  hobby.style.backgroundImage = elem.getAttribute("img-url");
+  var hobbies = hobby.getElementsByClassName("hobby-txt");
+
+  for (var i = 0; i < hobbies.length; i++) {
+    if (hobbies[i].className.includes("active")) {
+      hobbies[i].className = hobbies[i].className.replace(" active", "");
+    }
+  }
+  elem.className += " active";
+}
