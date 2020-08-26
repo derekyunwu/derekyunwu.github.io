f6820d5bc810fb17c3dcf1378b1781d605b4e906
derekyunwu <46233349+derekyunwu@users.noreply.github.com>
2020-08-25 19:23:34 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+var prev = window.pageYOffset;
+var nav = document.getElementById("navbar");
+window.onscroll = function () {
+  var curr = window.pageYOffset;
+  if (prev > curr) {
+    nav.style.top = "0";
+  } else {
+    nav.style.top = "-50px";
+  }
+  prev = curr;
+};
+
 function showMoreSkills() {
   var buttonText = document.getElementById("morebutton");
   var moreSkills = document.getElementById("more");
@@ -13,6 +25,22 @@ function showMoreSkills() {
     directionIcon.setAttribute("name", "chevron-up-outline");
   }
 }
+
+// function showMoreSkills() {
+//   var buttonText = document.getElementById("morebutton");
+//   var moreSkills = document.getElementById("more");
+//   var directionIcon = document.getElementById("direction-icon");
+
+//   if (moreSkills.style.maxHeight) {
+//     moreSkills.style.maxHeight = null;
+//     buttonText.innerHTML = "View More";
+//     directionIcon.setAttribute("name", "chevron-down-outline");
+//   } else {
+//     moreSkills.style.maxHeight = moreSkills.scrollHeight + "px";
+//     buttonText.innerHTML = "View Less";
+//     directionIcon.setAttribute("name", "chevron-up-outline");
+//   }
+// }
 
 function badBackground(elem) {
   var hobby = document.getElementById("hobby");
